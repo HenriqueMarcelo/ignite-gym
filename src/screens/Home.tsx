82adc1289467fa6a6343de8abc0c63fbc9064753
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-06 16:03:22 -0300

    style: show group selected

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,26 @@
+import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { VStack } from 'native-base'
+import { HStack, VStack } from 'native-base'
+import { useState } from 'react'
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costa')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <HStack>
+        <Group
+          name="costa"
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group
+          name="ombro"
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
     </VStack>
   )
 }
