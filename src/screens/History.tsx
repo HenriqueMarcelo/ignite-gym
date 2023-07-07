45f86d9815684344fa23536142dcb3a9b2579a02
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-07 11:26:56 -0300

    feat: use SectionList in history screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,15 +1,42 @@
 import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from 'native-base'
+import { Heading, SectionList, VStack } from 'native-base'
+
+const sections = [
+  {
+    title: '26.06.12',
+    data: ['Pizza', 'Burger', 'Risotto'],
+  },
+  {
+    title: '26.06.13',
+    data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+  },
+  {
+    title: '26.08.14',
+    data: ['Water', 'Coke', 'Beer'],
+  },
+  {
+    title: '14.12.23',
+    data: ['Cheese Cake', 'Ice Cream'],
+  },
+]
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de exercícios" />
 
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+      <SectionList
+        sections={sections}
+        keyExtractor={(item) => item}
+        renderItem={() => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color={'gray.200'} fontSize={'md'} mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
     </VStack>
   )
 }
