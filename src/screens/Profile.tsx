5c6000b42b3acce68d27957efaff8ffe21b9f9f3
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-07 14:03:00 -0300

    feat: use skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,35 @@
-import { Center, Text } from 'native-base'
+import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+import { VStack, Text, ScrollView, Center, Skeleton } from 'native-base'
+import { useState } from 'react'
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [photoIsLoadinng, setPhotoIsLoading] = useState(true)
+
   return (
-    <Center flex={1}>
-      <Text color="white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6}>
+          {photoIsLoadinng ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/HenriqueMarcelo.png' }}
+              alt=""
+              size={PHOTO_SIZE}
+            />
+          )}
+        </Center>
+      </ScrollView>
+    </VStack>
   )
 }
