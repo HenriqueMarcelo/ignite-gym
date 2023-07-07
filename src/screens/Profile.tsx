499786d4ee61dd383a880748e870537c82723684
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-07 14:10:09 -0300

    feat: use Input in Profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,17 +1,19 @@
+import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
 import { VStack, Text, ScrollView, Center, Skeleton } from 'native-base'
 import { useState } from 'react'
+import { TouchableOpacity } from 'react-native'
 
 const PHOTO_SIZE = 33
 
 export function Profile() {
-  const [photoIsLoadinng, setPhotoIsLoading] = useState(true)
+  const [photoIsLoadinng, _setPhotoIsLoading] = useState(true)
 
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
-      <ScrollView>
+      <ScrollView px={8}>
         <Center mt={6}>
           {photoIsLoadinng ? (
             <Skeleton
@@ -28,6 +30,25 @@ export function Profile() {
               size={PHOTO_SIZE}
             />
           )}
+          <TouchableOpacity>
+            <Text
+              color="green.600"
+              fontWeight={'bold'}
+              fontSize={'md'}
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input placeholder="Nome" bg="gray.600" />
+          <Input
+            placeholder="E-mail"
+            value="mhmsn@hotmail.com"
+            bg="gray.600"
+            isDisabled
+          />
         </Center>
       </ScrollView>
     </VStack>
