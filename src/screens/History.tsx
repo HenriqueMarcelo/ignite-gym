9ab3f0d309833af5a357149878d0b4d2e5813c35
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-07 12:03:56 -0300

    feat: history screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,6 +1,6 @@
 import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
-import { Heading, SectionList, VStack } from 'native-base'
+import { Heading, SectionList, Text, VStack } from 'native-base'
 
 const sections = [
   {
@@ -29,11 +29,24 @@ export function History() {
       <SectionList
         sections={sections}
         keyExtractor={(item) => item}
+        showsVerticalScrollIndicator={false}
         renderItem={() => <HistoryCard />}
         renderSectionHeader={({ section }) => (
           <Heading color={'gray.200'} fontSize={'md'} mt={10} mb={3}>
             {section.title}
           </Heading>
+        )}
+        contentContainerStyle={
+          !sections.length && {
+            flex: 1,
+            justifyContent: 'center',
+          }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign={'center'}>
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercicios hoje?
+          </Text>
         )}
         px={8}
       />
