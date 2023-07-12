3d3cd84b6564913a9b9596b9f2a3620696bd85d1
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-12 15:53:44 -0300

    core: fix native base bug

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { LogBox } from 'react-native'
 import {
   useFonts,
   Roboto_400Regular,
@@ -11,10 +10,6 @@ import { Loading } from '@components/Loading'
 import { THEME } from './src/theme'
 import { Routes } from '@routes/index'
 import { AuthContextProvider } from '@contexts/AuthContext'
-
-LogBox.ignoreLogs([
-  'In React 18, SSRProvider is not necessary and is a noop. You can remove it from your app.',
-])
 
 export default function App() {
   const [frontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
