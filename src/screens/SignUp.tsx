6c93d06e59140046ad418f5a556bce3004f5a74f
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-12 16:28:15 -0300

    feat: redirect after signUp

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -17,6 +17,7 @@ import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { api } from '@services/api'
 import { AppError } from '@utils/AppError'
+import { useAuth } from '@hooks/useAuth'
 
 type FormDataProps = {
   name: string
@@ -41,20 +42,20 @@ const signUpSchema = yup.object({
 export function SignUp() {
   const navigation = useNavigation()
   const toast = useToast()
+  const { signIn } = useAuth()
 
   const {
     control,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema),
   })
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
     try {
-      const response = await api.post('/users', { name, email, password })
-
-      console.log('data -> ', response)
+      await api.post('/users', { name, email, password })
+      await signIn(email, password)
     } catch (error) {
       const isAppError = error instanceof AppError
       const title = isAppError
@@ -160,6 +161,7 @@ export function SignUp() {
           <Button
             title="Criar e acessar"
             onPress={handleSubmit(handleSignUp)}
+            isLoading={isSubmitting}
           />
         </Center>
         <Button
