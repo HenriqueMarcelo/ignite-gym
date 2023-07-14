516d9169449c99e8f429fa37f0eb5d776a80515d
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-14 15:30:28 -0300

    fix: get new token using refresh_token

--- src/services/api.ts
+++ src/services/api.ts
@@ -20,14 +20,14 @@ const api = axios.create({
   baseURL: 'http://192.168.0.169:3333',
 }) as APIInstanceProps
 
-let failedQueue: PromiseType[] = []
+let failedQueued: PromiseType[] = []
 let isRefreshing = false
 
 api.registerInterceptTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
     (response) => response,
     async (requestError) => {
-      if (requestError?.response?.status === 401) {
+      if (requestError.response?.status === 401) {
         if (
           requestError.response.data?.message === 'token.expired' ||
           requestError.response.data?.message === 'token.invalid'
@@ -43,14 +43,14 @@ api.registerInterceptTokenManager = (signOut) => {
 
           if (isRefreshing) {
             return new Promise((resolve, reject) => {
-              failedQueue.push({
-                onSuccess: (token) => {
+              failedQueued.push({
+                onSuccess: (token: string) => {
                   originalRequestConfig.headers = {
                     Authorization: `Bearer ${token}`,
                   }
                   resolve(api(originalRequestConfig))
                 },
-                onFailure: (error) => {
+                onFailure: (error: AxiosError) => {
                   reject(error)
                 },
               })
@@ -71,22 +71,23 @@ api.registerInterceptTokenManager = (signOut) => {
                 originalRequestConfig.data = JSON.parse(
                   originalRequestConfig.data,
                 )
-
-                originalRequestConfig.headers = {
-                  Authorization: `Bearer ${data.token}`,
-                }
-                api.defaults.headers.common.Authorization = `Bearer ${data.token}`
-
-                failedQueue.forEach((request) => {
-                  request.onSuccess(data.token)
-                })
-
-                console.log('Token Atualizado!')
-
-                resolve(api(originalRequestConfig))
               }
+
+              originalRequestConfig.headers = {
+                Authorization: `Bearer ${data.token}`,
+              }
+              api.defaults.headers.common.Authorization = `Bearer ${data.token}`
+
+              failedQueued.forEach((request) => {
+                request.onSuccess(data.token)
+              })
+
+              console.log('Token Atualizado!')
+
+              resolve(api(originalRequestConfig))
             } catch (error: any) {
-              failedQueue.forEach((request) => {
+              console.log(error)
+              failedQueued.forEach((request) => {
                 request.onFailure(error)
               })
 
@@ -94,7 +95,7 @@ api.registerInterceptTokenManager = (signOut) => {
               reject(error)
             } finally {
               isRefreshing = false
-              failedQueue = []
+              failedQueued = []
             }
           })
         }
@@ -104,8 +105,9 @@ api.registerInterceptTokenManager = (signOut) => {
 
       if (requestError.response && requestError.response.data) {
         return Promise.reject(new AppError(requestError.response.data.message))
+      } else {
+        return Promise.reject(requestError)
       }
-      return Promise.reject(requestError)
     },
   )
 
